50ef108bf6fdd0e081064ba9d625d377de0ec5d6
Samuel Legrain <samuel.legrain@bstorm.be>
2020-07-13 10:24:49 +0200

    Init db

---
+++ DB_Bateau/Table/User.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT NOT NULL PRIMARY KEY UNIQUE	--uk_user_id_6FE82AFD --Ne fonctionne que sur une colonne
+	CONSTRAINT UK_User_Id UNIQUE (Id,Name)		--UK_User_Id
+)
